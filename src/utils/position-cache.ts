89f4597135ba4304f2161377ede669ba70e4e212
Ori Segal <osegal@salesforce.com>
2026-03-17 20:15:46 +0200

    Update package metadata and improve code organization
    - Added "preview" flag to package.json for feature toggling.
    - Included copyright headers in multiple source files for compliance.
    - Ensured consistent formatting by adding newlines at the end of files.
    - Enhanced overall code organization and readability across various modules.

--- src/utils/position-cache.ts
+++ src/utils/position-cache.ts
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root
+ */
+
 /**
  * Position Cache Utility for ERD Node Positions
  * Generates JavaScript code to be injected into webviews for position caching via extension storage.
--- src/webviews/index.ts
+++ src/webviews/index.ts
@@ -1,2 +1,9 @@
+/*
+ * Copyright (c) 2025, salesforce.com, inc.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ * For full license text, see the LICENSE file in the repo root
+ */
+
 export { getOrgInfoWebviewContent } from './org-info';
 export { getModelsWebviewContent } from './models-list';
